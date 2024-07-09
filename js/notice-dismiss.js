537f8eb3a61d06462da6a92ccc89e2b19363aef4
John Cris <jc.lasta@searix.net>
2024-07-09 21:52:49 +0800

    Updated formatting to match the WordPress-Core coding standards. Added phpcs, phpcbf and WPCS.

--- js/notice-dismiss.js
+++ js/notice-dismiss.js
@@ -1,17 +1,22 @@
 ( function ( $ ) {
-    $( document ).on( 'click', '#last-modified-posts.notice.is-dismissible .notice-dismiss', function () {
-        $.ajax( {
-            url: ajaxurl,
-            method: 'POST',
-            data: {
-                action: 'dismiss_last_modified_posts_notice'
-            },
-            success: function ( response ) {
-                if ( response.success ) {
-                    console.log( 'Last modified and published posts notice dismissed.' );
-                }
-            }
-        } );
-    } );
-} ) ( jQuery );
-
+	$( document ).on(
+		'click',
+		'#last-modified-posts.notice.is-dismissible .notice-dismiss',
+		function () {
+			$.ajax(
+				{
+					url: ajaxurl,
+					method: 'POST',
+					data: {
+						action: 'dismiss_last_modified_posts_notice'
+					},
+					success: function ( response ) {
+						if ( response.success ) {
+							console.log( 'Last modified and published posts notice dismissed.' );
+						}
+					}
+				}
+			);
+		}
+	);
+} )( jQuery );
